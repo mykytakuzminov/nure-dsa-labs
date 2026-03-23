e1d5829ca7d7f2a0b54f656ecb14282c4ec78b33
Mykyta Kuzminov <mykytakuzminov@gmail.com>
2026-03-23 10:23:04 +0100

    feat(lab01): implement BST remove operation

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -25,14 +25,21 @@ public:
         InsertRecursive(root, value);
     }
 
+    void Remove(int value)
+    {
+        RemoveRecursive(root, value);
+    }
+
     void Print()
     {
         PrintRecursive(root);
+        std::cout << std::endl;
     }
 
     void Print(int low, int high)
     {
         PrintRecursive(root, low, high);
+        std::cout << std::endl;
     }
 
 private:
@@ -51,6 +58,46 @@ private:
         if (value > node->value)
         {
             InsertRecursive(node->right, value);
+        }
+    }
+
+    void RemoveRecursive(Node *&node, int value)
+    {
+        if (node == nullptr) return;
+
+        if (value < node->value)
+        {
+            RemoveRecursive(node->left, value);
+        }
+        else if (value > node->value)
+        {
+            RemoveRecursive(node->right, value);
+        }
+        else
+        {
+            if (node->left == nullptr && node->right == nullptr)
+            {
+                delete node;
+                node = nullptr;
+            }
+            else if (node->left == nullptr)
+            {
+                Node *temp = node;
+                node = node->right;
+                delete temp;
+            }
+            else if (node->right == nullptr)
+            {
+                Node *temp = node;
+                node = node->left;
+                delete temp;
+            }
+            else
+            {
+                Node* minRightNode = FindMin(root->right);
+                node->value = minRightNode->value;
+                RemoveRecursive(node->right, minRightNode->value);
+            }
         }
     }
 
@@ -83,18 +130,31 @@ private:
             PrintRecursive(node->right, low, high);
         }
     }
+
+    Node* FindMin(Node *node)
+    {
+        while (node->left != nullptr)
+            node = node->left;
+        return node;
+    }
 };
 
 int main()
 {
-    BinarySearchTree tree = BinarySearchTree();
+    BinarySearchTree bst = BinarySearchTree();
 
     int arr[10] = {30, 40, 50, 24, 8, 58, 48, 26, 11, 13};
 
-    for (int i = 0; i < 10; i++) tree.Insert(arr[i]);
+    for (int i = 0; i < 10; i++) bst.Insert(arr[i]);
 
-    tree.Print();
-    std::cout << "" << std::endl;
-    tree.Print(12, 45);
+    bst.Print();
+    bst.Print(12, 45);
+    bst.Remove(48);
+    bst.Print();
+    bst.Remove(11);
+    bst.Print();
+    bst.Remove(8);
+    bst.Print();
+
     return 0;
 }
